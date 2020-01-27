0bd28a1dfd63c38c6b4980a3a7b21d48349bbbc3
glushakova <glushakovanastya@gmail.com>
2020-01-27 23:27:50 +0300

    slick 2

--- js/main.js
+++ js/main.js
@@ -1,17 +1,43 @@
-var burger = document.getElementById("burger");
-var menu = document.getElementById("menu");
-var bg = document.getElementById("bg");
+// var burger = document.getElementById("burger");
+// var menu = document.getElementById("menu");
+// var bg = document.getElementById("bg");
 
-burger.addEventListener("click", function() {
-  var isCross = this.classList.contains("cross");
+// burger.addEventListener("click", function() {
+//   var isCross = this.classList.contains("cross");
 
-  if (isCross) {
-    this.classList.remove("cross");
-    menu.classList.remove("visible");
-    bg.classList.remove("visible");
-  } else {
-    this.classList.add("cross");
-    menu.classList.add("visible");
-    bg.classList.add("visible");
-  }
+//   if (isCross) {
+//     this.classList.remove("cross");
+//     menu.classList.remove("visible");
+//     bg.classList.remove("visible");
+//   } else {
+//     this.classList.add("cross");
+//     menu.classList.add("visible");
+//     bg.classList.add("visible");
+//   }
+// });
+
+$(".customer-review").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 1201,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 577,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
